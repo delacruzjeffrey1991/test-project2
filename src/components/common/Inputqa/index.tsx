cd19bbea02eaf2e644e3a8c76c0878d93a7f131f
Adriane James J. Puzon <adrianejames27@gmail.com>
2022-11-24 20:31:14 +0800

    refactor: Inputqa

--- src/components/common/Inputqa/index.tsx
+++ src/components/common/Inputqa/index.tsx
@@ -1,34 +1,34 @@
 import React from "react";
-import Styles from './styles.module.scss';
+import Styles from "./styles.module.scss";
 import { useState, useEffect, ChangeEvent } from "react";
 
-function Inputqa(
-    {
-        variant = 'white',
-        type='text',
-        color = 'gray',
-        ...props
-    }
-) {
-    //const [firstChildName, setFirstChildName] = useState<string>('')
+function Inputqa({
+  variant = "white",
+  type = "text",
+  color = "gray",
+  ...props
+}) {
+  //const [firstChildName, setFirstChildName] = useState<string>('')
 
-    //useEffect(() => {
-    //    setFirstChildName(props.name)
-    //}, [props.name])
+  //useEffect(() => {
+  //    setFirstChildName(props.name)
+  //}, [props.name])
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    //setFirstChildName(event.target.value);
+    props.updatePostText(event.target.value);
+  };
 
-        //setFirstChildName(event.target.value);
-        props.updatePostText(event.target.value);
-    };
-
-
-    return (
-        <>
-            <input type={type} placeholder={`${props.placeholder}`} className={` ${Styles.inpuField} ${Styles[variant]} ${Styles[color]}`} onChange={handleChange} />
-        </>
-    );
+  return (
+    <>
+      <input
+        type={type}
+        placeholder={`${props.placeholder}`}
+        className={` ${Styles.inpuField} ${Styles[variant]} ${Styles[color]}`}
+        onChange={handleChange}
+      />
+    </>
+  );
 }
-
 
 export default Inputqa;
